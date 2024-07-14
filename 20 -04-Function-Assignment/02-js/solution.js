f70a14212c3518c35d88b88f1b94c59e99592885
gsharma03 <gauravsh.9315@gmail.com>
2024-07-14 21:56:42 +0530

    completed assignments

---
+++ 20 -04-Function-Assignment/02-js/solution.js
@@ -0,0 +1,7 @@
+function generateGreeting(name = "Dear") {
+  console.log(`\nHello ${name}, Welcome to PW skils.`);
+}
+generateGreeting();
+generateGreeting("Gaurav");
+generateGreeting("Vinay");
+generateGreeting("Shanket");
---
+++ 20 -04-Function-Assignment/03-js/solution.js
@@ -0,0 +1,7 @@
+
+(function(){
+    function square(value){
+        console.log(value**2);
+    }
+    square(5);
+})()
